e53b453f1ffefaa8a403b64612903d52db9780de
Psycho-cracker <93853227+Psycho-cracker@users.noreply.github.com>
2022-02-02 11:10:18 +0530

    Add files via upload

---
+++ userbot/helpers/utils/tools.py
@@ -0,0 +1,88 @@
+import os
+from typing import Optional
+
+from moviepy.editor import VideoFileClip
+from PIL import Image
+
+from ...funcs.logger import logging
+from ...funcs.managers import edit_or_reply
+from ..tools import media_type
+from .utils import runcmd
+
+LOGS = logging.getLogger(__name__)
+
+
+async def media_to_pic(event, reply, noedits=False):  # sourcery no-metrics
+    mediatype = media_type(reply)
+    if mediatype not in [
+        "Photo",
+        "Round Video",
+        "Gif",
+        "Sticker",
+        "Video",
+        "Voice",
+        "Audio",
+        "Document",
+    ]:
+        return event, None
+    if not noedits:
+        hinduevent = await edit_or_reply(
+            event, "`Transfiguration Time! Converting to ....`"
+        )
+
+    else:
+        hinduevent = event
+    hindumedia = None
+    hindufile = os.path.join("./temp/", "meme.png")
+    if os.path.exists(hindufile):
+        os.remove(hindufile)
+    if mediatype == "Photo":
+        hindumedia = await reply.download_media(file="./temp")
+        im = Image.open(hindumedia)
+        im.save(hindufile)
+    elif mediatype in ["Audio", "Voice"]:
+        await event.client.download_media(reply, hindufile, thumb=-1)
+    elif mediatype == "Sticker":
+        hindumedia = await reply.download_media(file="./temp")
+        if hindumedia.endswith(".tgs"):
+            hinducmd = f"lottie_convert.py --frame 0 -if lottie -of png '{hindumedia}' '{hindufile}'"
+            stdout, stderr = (await runcmd(hinducmd))[:2]
+            if stderr:
+                LOGS.info(stdout + stderr)
+        elif hindumedia.endswith(".webp"):
+            im = Image.open(hindumedia)
+            im.save(hindufile)
+    elif mediatype in ["Round Video", "Video", "Gif"]:
+        await event.client.download_media(reply, hindufile, thumb=-1)
+        if not os.path.exists(hindufile):
+            hindumedia = await reply.download_media(file="./temp")
+            clip = VideoFileClip(media)
+            try:
+                clip = clip.save_frame(hindufile, 0.1)
+            except Exception:
+                clip = clip.save_frame(hindufile, 0)
+    elif mediatype == "Document":
+        mimetype = reply.document.mime_type
+        mtype = mimetype.split("/")
+        if mtype[0].lower() == "image":
+            hindumedia = await reply.download_media(file="./temp")
+            im = Image.open(hindumedia)
+            im.save(hindufile)
+    if hindumedia and os.path.lexists(hindumedia):
+        os.remove(hindumedia)
+    if os.path.lexists(hindufile):
+        return hinduevent, hindufile, mediatype
+    return hinduevent, None
+
+
+async def take_screen_shot(
+    video_file: str, duration: int, path: str = ""
+) -> Optional[str]:
+    thumb_image_path = path or os.path.join(
+        "./temp/", f"{os.path.basename(video_file)}.jpg"
+    )
+    command = f"ffmpeg -ss {duration} -i '{video_file}' -vframes 1 '{thumb_image_path}'"
+    err = (await runcmd(command))[1]
+    if err:
+        LOGS.error(err)
+    return thumb_image_path if os.path.exists(thumb_image_path) else None
