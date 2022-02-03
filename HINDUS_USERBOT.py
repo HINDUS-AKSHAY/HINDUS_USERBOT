bc28d939ce30f25004c8cb6866e8472c75b03ff7
HINDUS AKSHAY <93853227+HINDUS-AKSHAY@users.noreply.github.com>
2022-02-03 10:18:40 +0530

    Update HINDUS_USERBOT.py

--- HINDUS_USERBOT.py
+++ HINDUS_USERBOT.py
@@ -6,7 +6,7 @@ logging.basicConfig(
     format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=logging.INFO
 )
 # Starting bot
-system("git clone https://gitHub.com/OFFICIAL-SAMEER/HINDUS USERBOT && cd HINDUS USERBOT && python3 -m userbot")
+system("git clone https://gitHub.com/HINDUS-AKSHAY/HINDUS_USERBOT && cd HINDUS_USERBOT && python3 -m userbot")
 # if got error
 print(" Sʀʏʏ ᴜɴғᴏʀᴛᴜɴᴀᴛᴇʟʏ ʏᴏᴜ ʜᴀᴠᴇ ɢᴏᴛ ᴇʀʀᴏʀ ᴘʟᴇᴀsᴇ ʀᴇᴘᴏʀᴛ ɪᴛ ᴀᴛ @HINDUS_USERBOT_SUPPORT")
 # Script written for stop heroku suspension
