cc70833c4ef7745e875153e3469e8e6a91c5cf34
HINDUS AKSHAY <93853227+HINDUS-AKSHAY@users.noreply.github.com>
2022-02-05 19:35:47 +0530

    Update hide.py

--- userbot/assistant/hide.py
+++ userbot/assistant/hide.py
@@ -15,7 +15,7 @@ async def on_plug_in_callback_query_handler(event):
         try:
             reply_pop_up_alert = jsondata[f"{timestamp}"]["text"]
         except KeyError:
-            reply_pop_up_alert = "This message no longer exists in hinduub server"
+            reply_pop_up_alert = "This message no longer exists in HINDUS_USERBOT server"
     else:
         reply_pop_up_alert = "This message no longer exists "
     await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
